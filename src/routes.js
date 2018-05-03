a2c387bd782cae30373e17c67d2e1a96f8fa3c1e
LeroyMin <minleeun06@gmail.com>
2018-05-03 23:03:28 +0800

    first commit

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import login from './views/login.vue'
+let routes = [
+    {
+        path: '/login',
+        component: login,
+        name: '',
+        hidden: true
+    }
+];
+
+export default routes;
