e01d642e4f63bf45061436c92a96dd2cf9465309
minliang <minliang@qtongx.com>
2018-10-28 22:04:21 +0800

    提交

--- src/components/main/main.vue
+++ src/components/main/main.vue
@@ -1,35 +1,32 @@
 <template>
 	<div>
-		<Layout>
-			<Header>
-				<Menu mode="horizontal" theme="dark">
-					<div class="layout-logo"></div>
-                    <div class="layout-nav">
-                    	<MenuItem>
-                    		 <Dropdown style="margin-left: 20px" placement="bottom-end" @on-click="handleClick">
-						      <Avatar>{{userName}}</Avatar>
-						        <DropdownMenu slot="list">
-						          <DropdownItem name="updatePassword">修改密码</DropdownItem>
-						          <DropdownItem name="logout">退出</DropdownItem>
-						        </DropdownMenu>
-						      </Dropdown>
-                    	</MenuItem>
-                    </div>
-				</Menu>
-			</Header>
-		</Layout>
-		<Layout>
-			<side-menu></side-menu>
-		<Layout>
-			<Breadcrumb></Breadcrumb>
-			<Content></Content>
-		</Layout>
+		<Layout>				
+			<head-bar></head-bar>
+			<Layout>
+				<side-menu></side-menu>
+				<Layout>
+					<Breadcrumb></Breadcrumb>
+					<Content></Content>
+				</Layout>
+			</Layout>
 		</Layout>
 	</div>
 </template>
 
 <script>
   import SideMenu from './side-menu/side-menu'
+  import HeadBar from './head-bar/head-bar'
+  export default {
+  	data () {
+  		return {
+  			
+  		}
+  	},
+  	components: {
+  		SideMenu,
+  		HeadBar
+  	}
+  }
 </script>
 
 <style></style>
---
+++ src/views/maino.vue
@@ -0,0 +1,55 @@
+<template>
+	<div class="layout">
+		<Layout>				
+			<head-bar></head-bar>
+			<Layout>
+				<side-menu></side-menu>
+				<Layout :style="{padding: '0 24px 24px'}">
+					<Breadcrumb :style="{margin: '24px 0'}"></Breadcrumb>
+					<Content :style="{padding: '24px', minHeight: '280px', background: '#fff'}"></Content>
+				</Layout>
+			</Layout>
+		</Layout>
+	</div>
+</template>
+
+<script>
+  import SideMenu from '_c/main/side-menu/side-menu'
+  import HeadBar from '_c/main/head-bar/head-bar'
+  export default {
+  	data () {
+  		return {
+  			
+  		}
+  	},
+  	components: {
+  		SideMenu,
+  		HeadBar
+  	}
+  }
+</script>
+
+<style scoped>
+.layout{
+    border: 1px solid #d7dde4;
+    background: #f5f7f9;
+    position: relative;
+    border-radius: 4px;
+    overflow: hidden;
+}
+.layout-logo{
+    width: 100px;
+    height: 30px;
+    background: #5b6270;
+    border-radius: 3px;
+    float: left;
+    position: relative;
+    top: 15px;
+    left: 20px;
+}
+.layout-nav{
+    width: 420px;
+    margin: 0 auto;
+    margin-right: 20px;
+}
+</style>
